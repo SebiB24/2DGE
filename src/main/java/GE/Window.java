f5038fdbbd6e4dc40d8e5c6d02b10277a1b8a69d
SebiB24 <sebycool242@gmail.com>
2025-02-14 01:53:55 +0200

    Key + Mouse Listeners

--- src/main/java/GE/Window.java
+++ src/main/java/GE/Window.java
@@ -1,16 +1,26 @@
 package GE;
 
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
 public class Window {
 
     private int width, height;
-    private String name;
+    private String title;
+    private long glfwWindow;
 
     private static Window window = null;
 
     private Window(){
         this.width = 1920;
         this.height = 1080;
-        this.name = "2D Game Engine";
+        this.title = "2D Game Engine";
     }
 
     public static Window get(){
@@ -18,5 +28,75 @@ public class Window {
             window = new Window();
         }
         return window;
+    }
+
+    public void run(){
+        System.out.println("Hello LWJGL " + Version.getVersion() + '!');
+
+        init();
+        loop();
+
+        // Freeing up memory that we access directly through LWJGL because java garbage collector does not affect it
+        // Free the memory
+        glfwFreeCallbacks(glfwWindow);
+        glfwDestroyWindow(glfwWindow);
+
+        //Terminate GLFW and free error callback
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+
+    public void init(){
+        //Setup error callback
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        //Initialize GLFW
+        if(!glfwInit()){
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+
+        //Configure GLFW
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+
+        //Create the window
+        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+        if(glfwWindow == NULL){
+            throw new IllegalStateException("Unable to create GLFW window");
+        }
+
+        //Make the OpenGl context current
+        glfwMakeContextCurrent(glfwWindow);
+
+        //Enable v-sync
+        glfwSwapInterval(1);
+
+        //Make the window visable
+        glfwShowWindow(glfwWindow);
+
+        // This line is critical for LWJGL's interoperation with GLFW's
+        // OpenGL context, or any context that is managed externally.
+        // LWJGL detects the context that is current in the current thread,
+        // creates the GLCapabilities instance and makes the OpenGL
+        // bindings available for use.
+        GL.createCapabilities();
+
+
+    }
+
+    public void loop(){
+        while(!glfwWindowShouldClose(glfwWindow)){
+
+            // Poll events
+            glfwPollEvents();
+
+            glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            // Swaping the fornt and back buffers (current displayed frame with newly generated frame)
+            glfwSwapBuffers(glfwWindow);
+        }
     }
 }
